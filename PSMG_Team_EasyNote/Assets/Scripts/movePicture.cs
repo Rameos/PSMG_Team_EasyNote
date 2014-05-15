587ea09b6cc9587cc4de1c4f7f101eb0e0e2ba12
JuPa303 <juliane.pawlow@stud.uni-regensburg.de>
2014-05-15 14:36:41 +0200

    scripts für Picture und onClicked

---
+++ PSMG_Team_EasyNote/Assets/Scripts/movePicture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePicture : MonoBehaviour {
+
+    void update()
+    {
+        
+
+
+    }
+    void OnMouseDown()
+    {
+        //transform.Translate(Vector3.left * 1);
+        Vector3 amountToMove = new Vector3(-1, 0, 0); //  Vector3.up is short hand for (0,1,0) if you want to use that instead
+
+            transform.position = Vector3.Lerp(transform.position, transform.position + amountToMove, 1);
+        
+
+    }
+	
+	
+}
---
+++ PSMG_Team_EasyNote/Assets/Scripts/onClicked.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onClicked : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        Debug.Log("Regal wurde geklickt");
+     
+    }
+ 
+
+}
