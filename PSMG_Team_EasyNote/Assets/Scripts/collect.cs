0ce535223fa65d2b8e3e39bda74d0dba1442352d
SFLde <francie.a@web.de>
2014-05-15 23:47:47 +0200

    test

---
+++ PSMG_Team_EasyNote/Assets/Scripts/collect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collect : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        Debug.Log("Objekt wird im Inventar plaziert");
+        Color greenColour = new Color(0, 102, 0);
+        renderer.material.color = greenColour;
+    }
+
+}
---
+++ PSMG_Team_EasyNote/Assets/Scripts/objectfound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectfound : MonoBehaviour {
+
+    void Start()
+    {
+        Debug.Log("Finde 5 Gegenstände!");
+    }
+
+    void OnMouseDown()
+    {
+        //if(int anzahlliste=0){
+
+            //anzahlliste++;
+        //}
+        Debug.Log("Du hast ein Objekt gefunden!");
+        Color greenColour = new Color(0, 102, 0);
+        renderer.material.color = greenColour;
+    }
+}
