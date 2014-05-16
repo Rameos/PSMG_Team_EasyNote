bb6f97ae7cc7a136926f66d8962f0de63099a3bb
JuPa303 <juliane.pawlow@stud.uni-regensburg.de>
2014-05-16 13:07:11 +0200

    animation script

--- PSMG_Team_EasyNote/Assets/Scripts/movePicture.cs
+++ PSMG_Team_EasyNote/Assets/Scripts/movePicture.cs
@@ -2,22 +2,18 @@
 using System.Collections;
 
 public class movePicture : MonoBehaviour {
-
-    void update()
-    {
-        
-
-
-    }
+    GameObject pic;
+ 
     void OnMouseDown()
     {
-        //transform.Translate(Vector3.left * 1);
-        Vector3 amountToMove = new Vector3(-1, 0, 0); //  Vector3.up is short hand for (0,1,0) if you want to use that instead
-
-            transform.position = Vector3.Lerp(transform.position, transform.position + amountToMove, 1);
-        
+        Debug.Log("OnMouseDown");
+         pic = GameObject.FindGameObjectWithTag("Bild");
+         Debug.Log("Play");
+         pic.animation.Play("move");
 
     }
+   
 	
 	
 }
+
