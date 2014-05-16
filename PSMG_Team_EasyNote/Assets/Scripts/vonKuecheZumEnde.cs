6d6cd7c91324c00ed16cb98269a9ae6546f67c4b
alwine3010 <alwine.hochschild@stud.uni-regensburg.de>
2014-05-16 09:42:45 +0200

    weitere Räume verbunden / Ende hinzugefügt

---
+++ PSMG_Team_EasyNote/Assets/Scripts/vonKuecheZumEnde.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vonKuecheZumEnde : MonoBehaviour {
+
+    //// Use this for initialization
+    //void Start () {
+	
+    //}
+	
+    //// Update is called once per frame
+    //void Update () {
+	
+    //}
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel(9);
+    }
+}
