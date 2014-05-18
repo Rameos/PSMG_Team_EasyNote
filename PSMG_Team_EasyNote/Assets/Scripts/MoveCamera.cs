f5d1734b02003783cf7c88b5722f7a1df8c07750
JuPa303 <juliane.pawlow@stud.uni-regensburg.de>
2014-05-18 18:59:55 +0200

    movePicture angepasst, kamerawechsel in Wohnzimmer

---
+++ PSMG_Team_EasyNote/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+    void start()
+    {
+        camSwap(1);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("1"))
+        {
+            Debug.Log("Using Camera One");
+            camSwap(1);
+        }
+        if (Input.GetKey("2"))
+        {
+            Debug.Log("Using Camera Two");
+            camSwap(2);
+        }
+
+        if (Input.GetKey("3"))
+        {
+            Debug.Log("Using Camera Three");
+            camSwap(3);
+        }
+
+      
+    }
+
+    void camSwap(int currentCam)
+    {
+        GameObject[] cameras = GameObject.FindGameObjectsWithTag("camera");
+
+        foreach (GameObject cams in cameras)
+        {
+            Camera theCam = cams.GetComponent<Camera>() as Camera;
+            theCam.enabled = false;
+        }
+
+        string oneToUse = "Camera" + currentCam;
+        Camera usedCam = GameObject.Find(oneToUse).GetComponent<Camera>() as Camera;
+        usedCam.enabled = true;
+    }
+}
--- PSMG_Team_EasyNote/Assets/Scripts/movePicture.cs
+++ PSMG_Team_EasyNote/Assets/Scripts/movePicture.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class movePicture : MonoBehaviour {
     GameObject pic;
+    bool picMoved = false;
  
     void OnMouseDown()
     {
-        Debug.Log("OnMouseDown");
-         pic = GameObject.FindGameObjectWithTag("Bild");
-         Debug.Log("Play");
-         pic.animation.Play("move");
-
+        if (picMoved == false)
+        {
+            Debug.Log("OnMouseDown");
+            pic = GameObject.FindGameObjectWithTag("Bild");
+            Debug.Log("Play");
+            pic.animation.Play("move");
+            picMoved = true;
+        }
+       
     }
    
 	
