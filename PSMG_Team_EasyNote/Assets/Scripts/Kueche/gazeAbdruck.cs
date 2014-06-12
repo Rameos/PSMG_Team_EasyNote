45b3cf35d889a164fb9a5193c908c7bdbf231446
alwine3010 <alwine.hochschild@stud.uni-regensburg.de>
2014-06-12 13:16:39 +0200

    eyetracker versuch

---
+++ PSMG_Team_EasyNote/Assets/Scripts/Kueche/gazeAbdruck.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using iViewX;
+
+public abstract class gazeAbdruck : MonoBehaviourWithGazeComponent
+{
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public override void OnGazeStay(RaycastHit hit)
+    {
+        Color greenColour = new Color(0, 102, 0);
+
+        renderer.material.color = greenColour;
+        //if(Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    //float r = Random.Range(0f,1f);
+        //    //float g = Random.Range(0f,1f);
+        //    //float b = Random.Range(0f,1f);
+        //    Color greenColour = new Color(0, 102, 0);
+
+        //    renderer.material.color = greenColour;
+        //}
+        
+    } 
+
+}
