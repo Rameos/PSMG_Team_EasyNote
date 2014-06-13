82a4c462fe5bf9cebef27777559e55e1450e2172
alwine3010 <alwine.hochschild@stud.uni-regensburg.de>
2014-06-13 12:05:47 +0200

    badteppich

---
+++ PSMG_Team_EasyNote/Assets/Scripts/Bad/verschiebeBadteppich.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class verschiebeBadteppich : MonoBehaviour {
+
+    //// Use this for initialization
+    //void Start () {
+	
+    //}
+	
+    //// Update is called once per frame
+    //void Update () {
+	
+    //}
+    void OnMouseDown()
+    {
+        transform.Translate(Vector3.right * 3);
+    }
+}
