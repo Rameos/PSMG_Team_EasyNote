1c7704efd592223a0b64970077847d9bd9fecd0f
SFLde <francie.a@web.de>
2014-08-28 15:55:50 +0200

    bsp

---
+++ PSMG_Team_EasyNote/Assets/Scripts/InputEinstellungen.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ Dieser Inputmanager löst nun entsprechend dem INput des Spielers/Nutzers die Events des StateManagers aus. Es ist Sinnvoll Input und Controller zu trennen, um schnell weitere inputmanager z.B. bauen zu können ohne die verarbeitende Mechanik zu verändern (= MVC)
+ */
+
+public class InputExample : MonoBehaviour
+{
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R)
+        {
+            Debug.Log("X-Ray Mode");
+            //Event wird nun gefeuert
+            StateManager.OnSetRoentgenModus();
+        }
+
+        else if (Input.GetKeyUp(KeyCode.R))
+        {
+            Debug.Log("X-Ray Mode stop");
+            //Event wird nun gefeuert
+            StateManager.OnSetNormalModus);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Debug.Log("Heatmap Mode");
+            //Event wird nun gefeuert
+            StateManager.OnSetUVModus();
+        }
+
+        else if (Input.GetKeyUp(KeyCode.U))
+        {
+            Debug.Log("Heatmap Mode stop");
+            //Event wird nun gefeuert
+            StateManager.OnSetNormalModus();
+        }
+    }
+}
---
+++ PSMG_Team_EasyNote/Assets/Scripts/StateManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*
+  Wir nutzen die Events dazu, um die verschiedenen Modi des Spiels zu triggern. Jedes Script kann über diese events angetriggert werden, ein bestimmtes Verhalten zu starten/beenden, wenn der modus gewechselt wird. 
+    (z.B. materialwechsel, Soundwechsel, effektstarts usw.)
+ */
+
+// Delegaten für die Events;
+public delegate void SetNormalModus();
+public delegate void SetRoentgenModus();
+public delegate void SetUVModus();
+
+
+public class StateManager : MonoBehaviour {
+
+    //Events, die abgefeuert werden, um die Materialien anzutriggern
+    public static event SetNormalModus SetNormalModusHandler;
+    public static event SetRoentgenModus SetRoentgenModusHandler;
+    public static event SetUVModus SetUVModusHandler;
+
+
+    /// <summary>
+    /// Start nutzen Wir, um alle Objekte einmal in den Normalzustand zu versetzen
+    /// </summary>
+    void Start()
+    {
+        OnSetNormalModus();
+    }
+
+
+    /// <summary>
+    /// Statische Funktionen, die extern aufgerufen werden, wenn der Modus gewechselt wird
+    /// </summary>
+    public static void OnSetNormalModus()
+    {
+        if (SetNormalModusHandler != null)
+        {
+            SetNormalModusHandler();
+        }
+    }
+
+    public static void OnSetRoentgenModus()
+    {
+        if (SetRoentgenModusHandler!= null)
+        {
+            SetRoentgenModusHandler();
+        }
+    }
+
+    public static void OnSetUVModus()
+    {
+        if (SetUVModusHandler != null)
+        {
+            SetUVModusHandler();
+        }
+    }
+}
