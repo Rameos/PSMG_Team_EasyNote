ddf711043547c5e9164b778889c743e5ff5fcee0
SFLde <francie.a@web.de>
2014-09-15 16:59:45 +0200

    bla

--- PSMG_Team_EasyNote/Assets/Scripts/Kinderzimmer/guiTextKiZ.cs
+++ PSMG_Team_EasyNote/Assets/Scripts/Kinderzimmer/guiTextKiZ.cs
@@ -19,9 +19,9 @@ public class guiTextKiZ : MonoBehaviour {
     void OnGUI()
     {
         GUI.Button(new Rect(Screen.width * 0.03f, Screen.height * 0.08f, Screen.width * 0.2f, Screen.height * 0.1f), "Finde die 5 gesuchten Gegenstände");
-        GUI.Button(new Rect(Screen.width * 0.6f, Screen.height * 0.08f, Screen.width * 0.2f, Screen.height * 0.1f), "Rubik's Cube");
-        GUI.Button(new Rect(Screen.width * 0.03f, Screen.height * 0.08f, Screen.width * 0.2f, Screen.height * 0.1f), "Dokumentstapel");
-        GUI.Button(new Rect(Screen.width * 0.03f, Screen.height * 0.08f, Screen.width * 0.2f, Screen.height * 0.1f), "Türkiser Stift");
-        GUI.Button(new Rect(Screen.width * 0.03f, Screen.height * 0.08f, Screen.width * 0.2f, Screen.height * 0.1f), "Gelbes Auto");
+        GUI.Button(new Rect(Screen.width * 0.8f, Screen.height * 0.08f, Screen.width * 0.2f, Screen.height * 0.1f), "Rubik's Cube");
+        GUI.Button(new Rect(Screen.width * 0.8f, Screen.height * 0.20f, Screen.width * 0.2f, Screen.height * 0.1f), "Dokumentstapel");
+        GUI.Button(new Rect(Screen.width * 0.8f, Screen.height * 0.30f, Screen.width * 0.2f, Screen.height * 0.1f), "Türkiser Stift");
+        GUI.Button(new Rect(Screen.width * 0.8f, Screen.height * 0.40f, Screen.width * 0.2f, Screen.height * 0.1f), "Gelbes Auto");
     }
 }
