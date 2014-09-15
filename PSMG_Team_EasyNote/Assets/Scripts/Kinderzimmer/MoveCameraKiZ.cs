286df0b9044db58fb942b3f3b1d3b6ca66468320
SFLde <francie.a@web.de>
2014-09-15 12:01:59 +0200

    KiZAnim

--- PSMG_Team_EasyNote/Assets/Scripts/Kinderzimmer/MoveCameraKiZ.cs
+++ PSMG_Team_EasyNote/Assets/Scripts/Kinderzimmer/MoveCameraKiZ.cs
@@ -13,26 +13,27 @@ public class MoveCameraKiZ : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
+        //(-1.5, 2.4, 0.09)
         if (Input.GetKey("1"))
         {
-            transform.position = new Vector3(-4, 3, 9); // Richtet Kamera an gewünschter Position aus
-            transform.rotation = new Quaternion(0, 10, 0, 1);
+            transform.position = new Vector3(0, 3, 0); // Richtet Kamera an gewünschter Position aus
+            transform.rotation = new Quaternion(0, -25, 5, 8);
         }
         if (Input.GetKey("2"))
         {
-            transform.position = new Vector3(-6, 2, 3);
-            transform.rotation = new Quaternion(0, 170, -51, 1);
+            transform.position = new Vector3(-1, 3, -3);
+            transform.rotation = new Quaternion(0, -20, 1, 40);
         }
         if (Input.GetKey("3"))
         {
-            transform.position = new Vector3(-6, 2, 3);
-            transform.rotation = new Quaternion(0, 170, 1, 51);
+            transform.position = new Vector3(1, 3, 3);
+            transform.rotation = new Quaternion(0, 270, -25, 0);
         }
         if (Input.GetKey("4"))
         {
-            transform.position = new Vector3(-1, 1, 1);
-            transform.rotation = new Quaternion(0, 80, 0, 80);
+            transform.position = new Vector3(-2, 2, 3);
+            transform.rotation = new Quaternion(0, 150, 0, 70);
         }
+        
     }
 }
