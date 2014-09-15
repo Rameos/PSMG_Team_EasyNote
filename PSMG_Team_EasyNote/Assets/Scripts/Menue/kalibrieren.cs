f3064f18ced66362cca59d5ce0f416fc20888801
alwine3010 <alwine.hochschild@stud.uni-regensburg.de>
2014-09-15 09:56:40 +0200

    kalibrieren

--- PSMG_Team_EasyNote/Assets/Scripts/Menue/kalibrieren.cs
+++ PSMG_Team_EasyNote/Assets/Scripts/Menue/kalibrieren.cs
@@ -1,19 +1,24 @@
 ﻿using UnityEngine;
+using System.Threading;
 using System.Collections;
+using iViewX;
 
-public class kalibrieren : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public class kalibrieren : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	    void OnClicked()
+	    {
+			GazeControlComponent.Instance.StartCalibration();
+		}
+
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-    void OnClicked()
-    {
-        //startCalibration();
-    }
-}
