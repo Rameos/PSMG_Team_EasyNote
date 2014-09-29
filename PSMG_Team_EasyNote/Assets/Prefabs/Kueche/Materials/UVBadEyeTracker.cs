47a037d93d28ad9fc44eca546b0d65856fa68255
alwine3010 <alwine.hochschild@stud.uni-regensburg.de>
2014-09-29 14:15:16 +0200

    scripte modi erstellt

---
+++ PSMG_Team_EasyNote/Assets/Prefabs/Kueche/Materials/UVBadEyeTracker.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using System.Collections;
+using iViewX;
+
+public class UVBadEyeTracker : MonoBehaviourWithGazeComponent
+{
+
+
+    private Vector3 startPos;
+    GameObject lamp;
+
+    // Use this for initialization
+    void Start()
+    {
+        lamp = GameObject.FindGameObjectWithTag("lampe");
+        gazeUI.Add(new GazeButton(new Rect(), "Cube"));
+
+
+    }
+
+
+    private ArrayList gazeUI = new ArrayList();
+
+    void Update()
+    {
+        MovingCamera movCam = Camera.main.GetComponent<MovingCamera>();
+        if (isDrawing)
+        {
+            foreach (GazeButton button in gazeUI)
+            {
+                button.Update();
+            }
+        }
+
+
+
+        Vector2 gazePos = (gazeModel.posGazeLeft + gazeModel.posGazeRight) * 0.5f;
+        gazePos.y = Screen.height - gazePos.y;
+
+
+
+        lamp.light.intensity = 0;
+        light.intensity = 1;
+
+
+
+/*
+        if (movCam.cameraPos == "Start")
+        {
+
+            lamp.light.intensity = 0;
+            light.intensity = 1;
+
+            Vector3 posUserGaze = new Vector3(gazePos.x, gazePos.y, 3);
+            posUserGaze = Camera.main.ScreenToWorldPoint(posUserGaze);
+
+            transform.position = posUserGaze;
+            transform.position = new Vector3(startPos.x, transform.position.y, transform.position.z);
+        }
+*/
+    }
+
+
+    void OnGUI()
+    {
+        if (isDrawing)
+        {
+            foreach (GazeButton button in gazeUI)
+            {
+                button.OnGUI();
+            }
+        }
+    }
+
+    public override void OnGazeStay(RaycastHit hit)
+    {
+
+
+    }
+
+    public bool isDrawing { get; set; }
+
+    public override void OnGazeEnter(RaycastHit hit)
+    {
+
+    }
+
+    public override void OnGazeExit()
+    {
+
+    }
+
+}
