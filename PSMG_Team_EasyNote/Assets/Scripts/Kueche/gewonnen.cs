7dffa41d567c844cd6c5aa7161ff65b4338698aa
alwine3010 <alwine.hochschild@stud.uni-regensburg.de>
2014-09-30 09:45:46 +0200

    bad und küche raumverknüpfung berichtigt

--- PSMG_Team_EasyNote/Assets/Scripts/Kueche/gewonnen.cs
+++ PSMG_Team_EasyNote/Assets/Scripts/Kueche/gewonnen.cs
@@ -11,7 +11,7 @@ public class gewonnen : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 				if (gewinnAbfrage.katzeStatus == 1) {
-			Application.LoadLevel(7);
+			Application.LoadLevel(4);
 				}
 		}
 }
